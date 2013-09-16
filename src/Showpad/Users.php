5ceed918704a8fa9828256194607f1c0e87ef543
Siesqo <jeroen@siesqo.be>
2013-09-16 17:15:38 +0200

    Users: create new user

--- src/Showpad/Users.php
+++ src/Showpad/Users.php
@@ -20,6 +20,43 @@ class ShowpadUsers
 	}
 
 	/**
+	 * Create user
+	 *
+	 * @param string $firstName First name of the user
+	 * @param string $lastName Last name of the user
+	 * @param string $email Email of the user
+	 * @param string $userName Unique name of the user within the organisation
+	 * @param string $language The user's language
+	 * @param string $userType[optional] Type of user, possibilities: 'owner', 'tablet' or 'admin'
+	 * @param string $isActive[optional] Indicates whether a user is active
+	 * @param string $password[optional] Password of the user (If no password is set, an invitation mail will be sent unless specified not to)
+	 * @param string $externalId[optional] External ID of the user as used in the source application
+	 * @param string $sendMailToUser[optional] Notify the user about this update
+	 * @return array
+	 */
+	public function create($firstName, $lastName, $email, $userName, $language,
+		$userType = 'tablet', $isActive = true, $password = null, $externalId = null, $sendMailToUser = false)
+	{
+		// init parameters
+		$params = array();
+
+		// define parameters
+		$params['firstName'] = (string) $firstName;
+		$params['lastName'] = (string) $lastName;
+		$params['email'] = (string) $email;
+		$params['userName'] = (string) $userName;
+		$params['language'] = (string) $language;
+		$params['userType'] = (string) $userType;
+		$params['isActive'] = (bool) $isActive;
+		if(isset($password)) $params['password'] = (string) $password;
+		if(isset($externalId)) $params['externalId'] = (string) $externalId;
+		$params['sendMailToUser'] = (bool) $sendMailToUser;
+
+		// return
+		return $this->master->doCall('users', $params);
+	}
+
+	/**
 	 * Get all users
 	 *
 	 * @param associative_array $params[optional]
